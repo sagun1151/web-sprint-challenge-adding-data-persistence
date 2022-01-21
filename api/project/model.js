1ae1712f7436505da36838fdd3c072388413a1a6
sagun-shrestha <sshrestha426699@gmail.com>
2022-01-21 15:45:42 -0700

    turned it into a desert

--- api/project/model.js
+++ api/project/model.js
@@ -3,41 +3,20 @@ const db = require("../../data/dbConfig");
 
 async function getProjects() {
   const data = await db("projects");
-  const newData = data.reduce((acc, step) => {
-    if (step.project_completed === 0) {
-      return acc.concat({
-        project_id: step.project_id,
-        project_name: step.project_name,
-        project_description: step.project_description,
-        project_completed: false,
-      });
-    } else {
-      return acc.concat({
-        project_id: step.project_id,
-        project_name: step.project_name,
-        project_description: step.project_description,
-        project_completed: true,
-      });
-    }
-  }, []);
-  return newData;
+  data.forEach((data) => (data.project_completed = !!data.project_completed))
+  
+  return data;
 }
 
 async function insert(data) {
   const [project_id] = await db("projects").insert(data);
-  const returns = await db("projects").where({ project_id }).first();
-  let value = false;
-  if (returns.project_completed === 1) {
-    value = true;
+  const obj = await db("projects").where({ project_id }).first();
+  
+  return {
+    ...obj,
+    project_completed: !! obj.project_completed
   }
-  const obj = {
-    project_id: returns.project_id,
-    project_name: returns.project_name,
-    project_description: returns.project_description,
-    project_completed: value,
-  };
 
-  return obj;
 }
 
 module.exports = {
--- api/task/model.js
+++ api/task/model.js
@@ -12,35 +12,15 @@ async function getTasks() {
       "project_name",
       "project_description"
     );
+    data.forEach((data) => (data.task_completed
+    = !!data.task_completed))
 
-  const newData = data.reduce((acc, step) => {
-    if (step.task_completed === 0) {
-      return acc.concat({
-        task_id: step.task_id,
-        task_description: step.task_description,
-        task_notes: step.task_notes,
-        task_completed: false,
-        project_name: step.project_name,
-        project_description: step.project_description,
-      });
-    } else {
-      return acc.concat({
-        task_id: step.task_id,
-        task_description: step.task_description,
-        task_notes: step.task_notes,
-        task_completed: true,
-        project_name: step.project_name,
-        project_description: step.project_description,
-      });
-    }
-  }, []);
-
-  return newData;
+  return data;
 }
 
 async function insert(data) {
   const [task_id] = await db("tasks").insert(data);
-  const returns = await db("tasks as t")
+  const obj = await db("tasks as t")
     .leftJoin("projects as p", "t.project_id", "p.project_id")
     .where("task_id", task_id)
     .select(
@@ -51,20 +31,26 @@ async function insert(data) {
       "t.project_id"
     )
     .first();
-  let value = false;
-  if (returns.task_completed === 1) {
-    value = true;
+  
+  return {
+    ...obj,
+    task_completed: !! obj.task_completed
   }
-  const obj = {
-    task_id: returns.task_id,
-    task_description: returns.task_description,
-    task_notes: returns.task_notes,
-    task_completed: value,
-    project_id: returns.project_id,
-  };
-
-  return obj;
 }
+//   let value = false;
+//   if (returns.task_completed === 1) {
+//     value = true;
+//   }
+//   const obj = {
+//     task_id: returns.task_id,
+//     task_description: returns.task_description,
+//     task_notes: returns.task_notes,
+//     task_completed: value,
+//     project_id: returns.project_id,
+//   };
+
+//   return obj;
+// }
 
 module.exports = {
   getTasks,
